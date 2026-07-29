28ccffa756ba45d6675e7a789aea865600e8b1f3
roidtaqi <roidtaqi@gmail.com>
2026-07-29 13:18:25 +0800

    lil fixes

--- tjoerah_mobile/lib/main.dart
+++ tjoerah_mobile/lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 import 'core/router/app_router.dart';
 import 'core/theme/app_theme.dart';
 import 'core/theme/theme_provider.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('id_ID');
   runApp(const ProviderScope(child: TjoerahPosApp()));
 }
 
